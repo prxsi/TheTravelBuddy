8ade695472a5207330d2875a96e525f2807dd547
Prasidika Basnet <prasidikam@gmail.com>
2024-07-30 09:50:53 -0400

    Initial Commit

---
+++ TheTravelBuddy/TheTravelBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTravelBuddyApp.swift
+//  TheTravelBuddy
+//
+//  Created by Scholar on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TheTravelBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
